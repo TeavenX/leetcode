d015386f8f79c23cd16f36d711118746b190d2a0
Teaven <1541661741@qq.com>
2023-03-12 01:55:34 +0800

    20230312

--- 搜索旋转排序数组.go
+++ 搜索旋转排序数组.go
@@ -3,11 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{4, 5, 6, 7, 0, 1, 2}
-	target := 6
-	//nums = []int{3, 1}
-	//target = 1
-	fmt.Println(search20220521(nums, target))
+	// nums := []int{4, 5, 6, 7, 0, 1, 2}
+	// target := 0
+	nums := []int{8, 9, 2, 3, 4}
+	target := 9
+	// nums := []int{4, 5, 6, 7, 0, 1, 2, 3}
+	// target := 0
+	// nums := []int{4, 5, 6, 7, 0, 1, 2}
+	// target := 3
+	// nums := []int{3, 1}
+	// target := 1
+	// nums := []int{1, 3}
+	// target := 3
+	// nums := []int{1}
+	// target := 1
+	// nums := []int{1}
+	// target := 0
+	// fmt.Println(search20220521(nums, target))
+	fmt.Println(search20230312(nums, target))
 }
 
 func search(nums []int, target int) int {
@@ -73,6 +86,73 @@ func search20220521(nums []int, target int) int {
 					right = mid - 1
 				}
 			}
+		}
+	}
+	return -1
+}
+
+func search20230311WA(nums []int, target int) int {
+	n := len(nums)
+	left, right := 0, n-1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] > nums[0] {
+			// mid落在左区间
+			if target > nums[0] && target < nums[mid] {
+				// target 也在左边，且target 比 mid要小，应该往左查找
+				right = mid - 1
+			} else {
+				// target在右边，或者target比mid要大
+				left = mid + 1
+			}
+		} else {
+			// mid落在右区间
+			if target < nums[0] && target > nums[mid] {
+				// target 也在右边，且target比mid要大，应该往右找
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
+
+func search20230312(nums []int, target int) int {
+	n := len(nums)
+	if n == 0 {
+		return -1
+	}
+	if n == 1 {
+		if nums[0] == target {
+			return 0
+		}
+		return -1
+	}
+	if nums[0] < nums[n-1] {
+		left, right := 0, n-1
+		for left <= right {
+			mid := left + (right-left)>>1
+			if nums[mid] == target {
+				return mid
+			} else if nums[mid] < target {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+		return -1
+	} else {
+		left := search(nums[:n>>1], target)
+		if left != -1 {
+			return left
+		}
+		right := search(nums[n>>1:], target)
+		if right != -1 {
+			return n>>1 + right
 		}
 	}
 	return -1
