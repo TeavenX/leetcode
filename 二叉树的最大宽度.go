776e7c49a711478e56b75fc221fe6e4c245f0ff8
Teaven <1541661741@qq.com>
2023-06-05 02:04:45 +0800

    20230605

--- 二叉树的最大宽度.go
+++ 二叉树的最大宽度.go
@@ -109,3 +109,74 @@ func widthOfBinaryTree(root *TreeNode) int {
 	dfs(root, 0, 1)
 	return maxLen
 }
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func widthOfBinaryTree(root *TreeNode) int {
+	cache := make(map[int]int)
+	ans := 0
+	var dfs func(node *TreeNode, idx, level int)
+	dfs = func(node *TreeNode, idx, level int) {
+		if node == nil {
+			return
+		}
+		if cache[level] == 0 {
+			cache[level] = idx
+		}
+		ans = max(ans, idx-cache[level]+1)
+		dfs(node.Left, idx*2, level+1)
+		dfs(node.Right, idx*2+1, level+1)
+	}
+	dfs(root, 1, 1)
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func widthOfBinaryTree(root *TreeNode) int {
+	root.Val = 1
+	q := []*TreeNode{root}
+	ans := 0
+	for len(q) > 0 {
+		size := len(q)
+		ans = max(ans, q[size-1].Val-q[0].Val+1)
+		for i := 0; i < size; i++ {
+			if q[i].Left != nil {
+				q[i].Left.Val = q[i].Val * 2
+				q = append(q, q[i].Left)
+			}
+			if q[i].Right != nil {
+				q[i].Right.Val = q[i].Val*2 + 1
+				q = append(q, q[i].Right)
+			}
+		}
+		q = q[size:]
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
