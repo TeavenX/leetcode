23482b594eab39fbca4311591a323bd576c0b998
Teaven <1541661741@qq.com>
2023-08-13 19:16:21 +0800

    20230813

--- 合并两个有序数组.go
+++ 合并两个有序数组.go
@@ -54,3 +54,18 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 		p2--
 	}
 }
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	i, j := m-1, n-1
+	k := len(nums1) - 1
+	for j >= 0 {
+		if i >= 0 && nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+		}
+		k--
+	}
+}
