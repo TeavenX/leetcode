f4c677e3a3e111b9a75810fc64de881971df9d71
Teaven <1541661741@qq.com>
2023-09-04 22:47:57 +0800

    20230904

--- 序列化和反序列化二叉搜索树.go
+++ 序列化和反序列化二叉搜索树.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"math"
 	"strconv"
 	"strings"
@@ -72,6 +73,71 @@ func (this *Codec) deserialize(data string) *TreeNode {
 func (this *Codec) atoi(a string) int {
 	result, _ := strconv.Atoi(a)
 	return result
+}
+
+/**
+ * Your Codec object will be instantiated and called as such:
+ * ser := Constructor()
+ * deser := Constructor()
+ * tree := ser.serialize(root)
+ * ans := deser.deserialize(tree)
+ * return ans
+ */
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type Codec struct {
+}
+
+func Constructor() Codec {
+	return Codec{}
+}
+
+// Serializes a tree to a single string.
+func (this *Codec) serialize(root *TreeNode) string {
+	data := make([]int, 0)
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		data = append(data, node.Val)
+		dfs(node.Left)
+		dfs(node.Right)
+	}
+	dfs(root)
+	bts, _ := json.Marshal(data)
+	return string(bts)
+}
+
+// Deserializes your encoded data to tree.
+func (this *Codec) deserialize(s string) *TreeNode {
+	data := make([]int, 0)
+	_ = json.Unmarshal([]byte(s), &data)
+	i := 0
+	var dfs func(mn, mx int) *TreeNode
+	dfs = func(mn, mx int) *TreeNode {
+		if i == len(data) {
+			return nil
+		}
+		x := data[i]
+		if x < mn || x > mx {
+			return nil
+		}
+		i++
+		root := &TreeNode{Val: x}
+		root.Left = dfs(mn, x)
+		root.Right = dfs(x, mx)
+		return root
+	}
+	return dfs(math.MinInt, math.MaxInt)
 }
 
 /**
